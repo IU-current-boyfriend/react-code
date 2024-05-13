fb7b2154846464fba05355bdb11c3808dfb2661f
zhengcong <zhengcong@linno.cn>
2024-05-13 16:16:38 +0800

    feature-完善devtools案例

--- day07/devtools/src/store/index.js
+++ day07/devtools/src/store/index.js
@@ -11,12 +11,78 @@ import { thunk } from 'redux-thunk';
 import HomeReducer from './home';
 import CalcultorReducer from './calcultor';
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true }) || compose;
+
+// const reducers = combineReducers({
+//   home: HomeReducer,
+//   calcultor: CalcultorReducer
+// });
+
+
+
+// const myCombineReducers = (
+//   reducersCollect
+// ) => {
+//   const mapReducersCollect = Object.entries(reducersCollect);
+//   const combination = (state = {}, actions) => {
+//     const mapReducersObj = {};
+//     mapReducersCollect.forEach(item => {
+//       const [reducerName, reducerFn] = item;
+//       mapReducersObj[reducerName] = reducerFn.call(null, state[reducerName], actions);
+//     });
+//     return mapReducersObj;
+//   }
+//   return combination;
+// }
+
+// const myCombineReducers = (reducersCollect) => {
+//   const combination = (state = {}, actions) => {
+//     const reducersMap = {};
+//     const reducersCollectMap = Object.entries(reducersCollect);
+//     reducersCollectMap.forEach(item => {
+//       const [name, reducerFn] = item;
+//       reducersMap[name] = reducerFn.call(null, state[name], actions);
+//     });
+//     /**
+//      * 数据格式是什么样子的？
+//      * {
+//      *  name: reducers.call() => state
+//      * }
+//      */
+//     return reducersMap;
+//   }
+//   return combination;
+// }
+
+// const reducers = myCombineReducers({
+//   home: HomeReducer,
+//   calcultor: CalcultorReducer
+// });
+
 
 const reducers = combineReducers({
   home: HomeReducer,
   calcultor: CalcultorReducer
 });
+
+// console.log('reducers: =>', reducers);
+
+
+/**
+ * 本质上你看reducers是函数形式:
+ * 
+ **/
+
+// const myCombineReducers = reducersCollect => {
+//   return () => {
+
+//   }
+// }
+
+// const reducers = myCombineReducers({
+//   home: HomeReducer,
+//   calcultor: CalcultorReducer
+// });
 
 
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
